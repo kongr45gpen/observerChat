b5aa5fe90bf3a04defbe467454c384d453e2e036
kongr45gpen <electrovesta@gmail.com>
2013-01-19 17:13:32 +0200

    General fixes

--- observerChat.cpp
+++ observerChat.cpp
@@ -3,6 +3,7 @@
 
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
+#include <iostream>
 
 class observerChat : public bz_Plugin
 {
@@ -11,26 +12,32 @@ class observerChat : public bz_Plugin
 
   virtual void Event ( bz_EventData *eventData );
 
-  const char* variableName = "_observerChat";
-  const char* defaultValue = "game";
-  const char* permName = "OBSERVERCHAT";
+  const char* variableName;
+  const char* defaultValue;
+  const char* permName ;
 };
 
 BZ_PLUGIN(observerChat)
 
 void observerChat::Init ( const char* commandLine )
 {
+  variableName = "_observerChat";
+  defaultValue = "game";
+  permName = "OBSERVERCHAT";
+
   int mode = (int)atoi(commandLine);
-    const char* variableValue = "";
-    if (mode == 0) variableValue = "off"; else
-    if (mode == 1) variableValue = "game"; else
-    if (mode == 2) variableValue = "alwaysOn"; else {
+  const char* variableValue = "game";
+    
+  if (mode == 0 && commandLine != NULL && strlen(commandLine) != 0) variableValue = "off";
+  else if (mode == 1) variableValue = "game"; 
+  else if (mode == 2) variableValue = "alwaysOn"; 
+  else {
     if(bz_BZDBItemExists(variableName))
       variableValue = bz_getBZDBString(variableName).c_str();
-  }
+    }
   bz_setBZDBString(variableName, variableValue);
-  bz_debugMessage(0,variableValue);
 
+  bz_debugMessage(0,variableValue);
   bz_debugMessage(4,"observerChat plugin loaded");
 
   Register(bz_eRawChatMessageEvent);
@@ -52,10 +59,10 @@ void observerChat::Event ( bz_EventData *eventData )
       
       std::string variableValue = makelower(bz_getBZDBString(variableName).c_str());
       if (variableValue == "off" || variableValue == "disable" || variableValue == "disabled" || variableValue == "no") return;
-      if (variableValue == "game" || variableValue == "ingame" || variableValue == "countdown" || variableValue == "active") {
-        duringTheGame = " during the game."; 
-	if (!bz_isCountDownActive()) return;
-      } 
+      else if (!(variableValue == "on" || variableValue == "alwayson" || variableValue == "enable" || variableValue == "enabled" || variableValue == "yes")) {
+        if (!bz_isCountDownActive()) return;
+        duringTheGame = " during the game";
+      }
             
       bz_sendTextMessagef(BZ_SERVER,from,"You are not allowed to send global messages%s.",duringTheGame);
       bz_sendTextMessagef(BZ_SERVER,from,"Please use observer chat only.");
